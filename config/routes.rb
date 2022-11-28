64066742514377444e54919f867825b6df1d63ec
Arindam Barman <arindambarmangithub@gmail.com>
2022-11-28 08:19:25 +0530

    add health endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get '/health', to: proc { [200, {}, ['success']] }
+
   post '/execute', to: 'graphql#execute'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
