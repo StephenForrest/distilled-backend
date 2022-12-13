2d144d7486b89e2bc433807ae15e921d1a8b3b80
Arindam Barman <arindambarmangithub@gmail.com>
2022-12-14 02:26:35 +0530

    Add some types

--- app/graphql/types/plan/goal_type.rb
+++ app/graphql/types/plan/goal_type.rb
@@ -14,6 +14,15 @@ module Types
       field :measurements_count, type: Integer, null: false
       field :success_criterias, type: [Types::Plan::SuccessCriteriaType], null: true
       field :completion, type: Float, null: false
+      field :success_criterias_count, type: Integer, null: true
+
+      def success_criterias_count
+        object.success_criterias.size
+      end
+
+      def recent_success_criterias
+        object.success_criterias.order(updated_at: :desc).limit(3)
+      end
     end
   end
 end
--- app/graphql/types/plan/plan_type.rb
+++ app/graphql/types/plan/plan_type.rb
@@ -7,9 +7,19 @@ module Types
       field :name, type: String, null: true
       field :uuid, type: String, null: false
       field :goals, type: [Types::Plan::GoalType], null: true
+      field :goals_count, type: Integer, null: true
+      field :recent_goals, type: [Types::Plan::GoalType], null: true
 
       def id
         object.uuid
+      end
+
+      def goals_count
+        object.goals.size
+      end
+
+      def recent_goals
+        object.goals.order(updated_at: :desc).limit(3)
       end
     end
   end
