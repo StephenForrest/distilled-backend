4cf5d7b073099be4a1a242faea2e4d22f60d3c60
James <james.bohrman@fermyon.com>
2023-01-07 00:02:05 -0500

    Setup rookout

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,11 @@
 
 require_relative 'boot'
 
+require 'rookout'
+
 require 'rails/all'
+
+::Rookout.start(token: ENV['ROOKOUT_TOKEN'])
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
