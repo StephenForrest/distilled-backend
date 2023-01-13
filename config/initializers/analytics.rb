df222393cefc17097c429cb4b801e2028b406f37
James <james.bohrman@fermyon.com>
2023-01-13 05:47:38 -0500

    Change structure

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -1,36 +1,33 @@
 require 'segment/analytics'
 
 module Analytics
-    class Track # Rubocop:disable Metrics/ClassLength
-        ALLOWED_EVENTS = [
-            Analytics::Event::User::SignedUp[:label],
-            Analytics::Event::User::SignedIn[:label],
-            Analytics::Event::Subscription::Created[:label],
-            Analytics::Event::Trial::Started[:label],
-            Analytics::Event::Trial::Ended[:label],
-            Analytics::Event::Subscription::Finalized[:label],
-            Analytics::Event::Subscription::Canceled[:label],
-            Analytics::Event::Subscription::Resumed[:label],
-            Analytics::Event::Subscription::Paid[:label],
-            Analytics::Event::Subscription::PaymentFailed[:label],
-            Analytics::Event::Subscription::PaymentSucceeded[:label],
-            Analytics::Event::Subscription::PaymentRefunded[:label],
-            Analytics::Event::Subscription::Updated[:label]
-        ].freeze
+    class Tracking # Rubocop:disable Metrics/ClassLength
         
         def initialize(context = {})
             @user = context[:user]
             @event = context[:event]
             @segment = Segment::Analytics.new(
-                write_key: ENV.fetch['SEGMENT_WRITE_KEY'],
-        end
-
-        def call
-            return unless @user && @event
-
-            @segment.track(
-                user_id: @user.id,
-                event: @event[:label],
-                properties: @event[:properties]
+                write_key: ENV.fetch('SEGMENT_WRITE_KEY'),
             )
         end
+        def call
+            @segment.track(
+                user_id: @user.id,
+                event: @event,
+                properties: properties,
+            )
+            @segment.identify(
+                user_id: @user.id,
+                traits: properties,
+            )
+        end
+        private def properties
+            {
+                email: @user.email,
+                name: @user.name,
+                created_at: @user.created_at,
+                updated_at: @user.updated_at,
+            }
+        end        
+    end
+end
