9f5dd6a87f4832b775ce071f1bffd1c049ec1efd
James <james.bohrman@fermyon.com>
2023-01-15 09:36:25 -0500

    Update analytics

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -1,41 +1,76 @@
+require 'segment/analytics'
+
 Analytics = Segment::Analytics.new({
     write_key: 'SEGMENT_WRITE_KEY',
     on_error: Proc.new { |status, msg| print msg }
-})
+    })
 
 Analytics.identify(
-    user_id: current_user.id,
+    user_id: 'current_user.id',
     traits: {
-      name: current_user.name,
-      email: current_user.email,
-      created_at: current_user.created_at,
-      profile_pic: current_user.profile_pic
-    })
+        email: 'current_user.email',
+        name: 'current_user.name'
+    }
+)
+
+Analytics.track(
+    user_id: 'current_user.id',
+    event: 'User Signed In',
+    properties: {
+        email: 'current_user.email',
+        name: 'current_user.name'
+    }
+)
+
+Analytics.track(
+    user_id: 'current_user.id',
+    event: 'User Signed Out',
+    properties: {
+        email: 'current_user.email',
+        name: 'current_user.name'
+    }
+)
+
+Analytics.track(
+    user_id: 'current_user.id',
+    event: 'User Created',
+    properties: {
+        email: 'current_user.email',
+        name: 'current_user.name'
+    }
+)
+
+Analytics.track(
+    user_id: 'current_user.id',
+    event: 'User Updated',
+    properties: {
+        email: 'current_user.email',
+        name: 'current_user.name'
+    }
+)
+
+Analytics.track(
+    user_id: 'current_user.id',
+    event: 'User Created Goal',
+    properties: {
+        email: 'current_user.email',
+        name: 'current_user.name'
+    }
+)
+
+Analytics.track(
+    user_id: 'current_user.id',
+    event: 'User Created Measurement',
+    properties: {
+        email: 'current_user.email',
+        name: 'current_user.name'
+    }
+)
 
 Analytics.group(
-    user_id: current_user.id,
-    group_id: current_user.workspaces.first.id,
+    user_id: 'current_user.id',
+    group_id: 'current_workspace.id',
     traits: {
-        name: current_user.workspaces.first.name,
-        created_at: current_user.workspaces.first.created_at
-    })
-
-Analytics.track(
-    user_id: current_user.id,
-    event: user_sessions.first.login_type,
-    properties: {
-        name: current_user.name,
-        email: current_user.email,
-        created_at: current_user.created_at,
-        profile_pic: current_user.profile_pic
-    })
-
-Analytics.track(
-    user_id: current_user.id,
-    event: user_sessions.SignOutMutation.session_id,
-    properties: {
-        name: current_user.name,
-        email: current_user.email,
-        created_at: current_user.created_at,
-        profile_pic: current_user.profile_pic
-    })
+        name: 'current_workspace.name'
+    }
+)
