a7f2c5e92c1f6dcffb2b1bcc5324568bcfe10bd6
atanych <atanych@gmail.com>
2023-01-22 06:41:08 +0300

    Add api_key

--- app/graphql/types/workspace/workspace_type.rb
+++ app/graphql/types/workspace/workspace_type.rb
@@ -8,10 +8,15 @@ module Types
       field :domain, type: String, null: true
       field :auto_join_from_domain, type: Boolean, null: false
       field :personal_domain, type: Boolean, null: false
+      field :api_key, type: String, null: false
       field :workspace_members, type: [Types::Workspace::WorkspaceMemberType], null: false
 
       def personal_domain
         object.public_domain?
+      end
+
+      def api_key
+        context[:current_user].workspace_members.find_by(workspace_id: object.id)&.api_key
       end
     end
   end
