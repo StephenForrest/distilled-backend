702d3a69f2b8be3ff2f54cb46488fb1430d09f56
atanych <atanych@gmail.com>
2023-01-25 13:27:39 +0300

    Fix typo

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -30,7 +30,7 @@ class StripeController < ApplicationController
       workspace.update!(stripe_product: event.data.object.plan.product)
       # sometimes stripe does not derirect back the customer and the customer needs to manually reopen our app
       # with the passed step "subscription"
-      Workspacess::OnboardingSteps.new(workspace).pass_onboarding_step('subscription')
+      Workspaces::OnboardingSteps.new(workspace).pass_onboarding_step('subscription')
     when 'charge.failed'
       Rails.logger.error("Stripe charge.failed for #{event.data.object.inspect}")
     when 'invoice.payment_failed'
