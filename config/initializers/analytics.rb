60fb8d51a79c2a53a3e65c61c34ce4e352823699
James <james.bohrman@fermyon.com>
2023-02-08 11:44:47 -0500

    analytics

--- config/initializers/analytics.rb
+++ config/initializers/analytics.rb
@@ -5,72 +5,72 @@ Analytics = Segment::Analytics.new({
                                      on_error: proc { |_status, msg| print msg }
                                    })
 
-# Analytics.identify(
-#   user_id: 'current_user.id',
-#   traits: {
-#     email: 'current_user.email',
-#     name: 'current_user.name'
-#   }
-# )
+ Analytics.identify(
+   user_id: user.id,
+   traits: {
+     email: current_user.email,
+     name: current_user.name
+   }
+ )
 
-# Analytics.track(
-#   user_id: 'current_user.id',
-#   event: 'User Signed In',
-#   properties: {
-#     email: 'current_user.email',
-#     name: 'current_user.name'
-#   }
-# )
+ Analytics.track(
+   user_id: user.id,
+   event: 'User Signed In',
+   properties: {
+     email: current_user.email,
+     name: current_user.name
+   }
+ )
 
-# Analytics.track(
-#   user_id: 'current_user.id',
-#   event: 'User Signed Out',
-#   properties: {
-#     email: 'current_user.email',
-#     name: 'current_user.name'
-#   }
-# )
+ Analytics.track(
+   user_id: current_user.id,
+   event: 'User Signed Out',
+   properties: {
+     email: current_user.email,
+     name: current_user.name
+   }
+ )
 
-# Analytics.track(
-#   user_id: 'current_user.id',
-#   event: 'User Created',
-#   properties: {
-#     email: 'current_user.email',
-#     name: 'current_user.name'
-#   }
-# )
+ Analytics.track(
+   user_id: current_user.id,
+   event: 'User Created',
+   properties: {
+     email: current_user.email,
+     name: current_user.name
+   }
+ )
 
-# Analytics.track(
-#   user_id: 'current_user.id',
-#   event: 'User Updated',
-#   properties: {
-#     email: 'current_user.email',
-#     name: 'current_user.name'
-#   }
-# )
+ Analytics.track(
+   user_id: current_user.id,
+   event: 'User Updated',
+   properties: {
+     email: current_user.email,
+     name: current_user.name
+   }
+ )
 
-# Analytics.track(
-#   user_id: 'current_user.id',
-#   event: 'User Created Goal',
-#   properties: {
-#     email: 'current_user.email',
-#     name: 'current_user.name'
-#   }
-# )
+ Analytics.track(
+   user_id: current_user.id,
+   event: 'User Created Goal',
+   properties: {
+     email: current_user.email,
+     name: current_user.name
+   }
+ )
 
-# Analytics.track(
-#   user_id: 'current_user.id',
-#   event: 'User Created Measurement',
-#   properties: {
-#     email: 'current_user.email',
-#     name: 'current_user.name'
-#   }
-# )
+ Analytics.track(
+   user_id: user.id,
+   event: 'User Created Measurement',
+   properties: {
+     email: current_user.email,
+     name: current_user.name
+   }
+ )
 
-# Analytics.group(
-#   user_id: 'current_user.id',
-#   group_id: 'current_workspace.id',
-#   traits: {
-#     name: 'current_workspace.name'
-#   }
-# )
+ Analytics.group(
+   user_id: 'current_user.id',
+   group_id: current_workspace.id,
+   traits: {
+     name: current_workspace.name
+   }
+ )
