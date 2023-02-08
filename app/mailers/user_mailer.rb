f6e7ff0abb08618694759ba93708ba7d3a23323d
James <james.bohrman@fermyon.com>
2023-02-08 18:07:05 -0500

    smtp

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserMailer < ApplicationMailer
-  default from: 'info@getdistilled.io'
+  default from: 'hello@getdistilled.io'
 
   def welcome_email
     @user = params[:user]
