465218c7bf4034b0269a37c30049a08274decf74
James <james.bohrman@fermyon.com>
2023-02-09 05:41:38 -0500

    Intercom

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'rails', '~> 7.0.4'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 1.1'
 
+gem "intercom-rails"
+
 # Use the Puma web server [https://github.com/puma/puma]
 gem 'puma', '~> 5.0'
 
--- app/graphql/mutations/create_goal_mutation.rb
+++ app/graphql/mutations/create_goal_mutation.rb
@@ -10,9 +10,31 @@ module Mutations
 
     def resolve(title:, plan_uuid:, expires_on:)
       plan = current_workspace.plans.find_by(uuid: plan_uuid)
-      goal = plan.goals.create!(title:, plan_id: plan.id, expires_on:, owner_id: current_user.id,
-                                workspace_id: current_workspace.id)
-      { goal: }
+      stripe_product = current_workspace.stripe_product
+      max_goals = stripe_product == 'free' ? 6 : Float::INFINITY
+    
+      if plan.goals.count >= max_goals
+        begin
+          Intercom::Client.new.messages.create(
+            from: { type: 'user', id: current_user.intercom_user_id },
+            body: "Bummer! You've reached the maximum number of goals allowed on the free plan 🥺. Ready to upgrade?"
+          )
+        rescue => e
+          Rails.logger.error("Intercom Error: #{e.inspect}")
+        end
+    
+        raise GraphQL::ExecutionError, "You have reached the maximum number of goals allowed on the free plan"
+      end
+    
+      goal = plan.goals.create!(
+        title: title, 
+        plan_id: plan.id, 
+        expires_on: expires_on, 
+        owner_id: current_user.id,
+        workspace_id: current_workspace.id
+      )
+    
+      { goal: goal }
     end
   end
 end
