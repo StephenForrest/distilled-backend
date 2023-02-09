996581489742609d376b0e2c3498295073a4c012
James <james.bohrman@fermyon.com>
2023-02-09 16:54:27 -0500

    workspace

--- app/graphql/mutations/create_workspace_mutation.rb
+++ app/graphql/mutations/create_workspace_mutation.rb
@@ -4,6 +4,7 @@ module Mutations
   class CreateWorkspaceMutation < BaseMutation
     argument :title, String, required: true
     argument :auto_join_from_domain, Boolean, required: true
+    argument :stripe_product, String, required: false
 
     field :workspace, type: Types::Workspace::WorkspaceType, null: false
 
@@ -11,12 +12,13 @@ module Mutations
       raise GraphQL::ExecutionError, 'Authentication expired' if context[:current_user].nil?
 
       workspace = context[:current_user].workspaces.create!(
-        title:,
-        auto_join_from_domain:,
-        domain: context[:current_user].user_domain
+        title: title,
+        auto_join_from_domain: auto_join_from_domain,
+        domain: context[:current_user].user_domain,
+        stripe_product: stripe_product,
       )
 
-      { workspace: }
+      { workspace: workspace }
     end
   end
 end
