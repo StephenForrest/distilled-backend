b4b956fb1c45522b52153bf16095f1a6a09273de
James <james.bohrman@fermyon.com>
2023-02-13 04:28:45 -0500

    errors

--- app/graphql/mutations/create_plan_mutation.rb
+++ app/graphql/mutations/create_plan_mutation.rb
@@ -9,7 +9,13 @@ module Mutations
     def resolve(name:)
       stripe_product = current_workspace.stripe_product
 
-
+      if stripe_product == ENV['STRIPE_FREE_PLAN_ID']
+        max_plans = 1 # 1 plan per workspace
+      elsif stripe_product == ENV['STRIPE_PRO_PLAN_ID']
+        max_plans = Float::INFINITY # unlimited plans per workspace
+      else
+        max_plans = 0 # no plans allowed
+      end
 
       puts current_workspace.stripe_product
       puts current_workspace.plans.count
