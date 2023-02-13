a45ff05ac5176dd81088f4dc47e30a2fa6007f74
James <james.bohrman@fermyon.com>
2023-02-13 07:20:28 -0500

    500

--- app/graphql/mutations/create_goal_mutation.rb
+++ app/graphql/mutations/create_goal_mutation.rb
@@ -9,25 +9,20 @@ module Mutations
     field :goal, Types::Plan::GoalType, null: false
 
     def resolve(title:, plan_uuid:, expires_on:)
+      plan = current_workspace.plans.find_by(uuid: plan_uuid)
       stripe_product = current_workspace.stripe_product
     
       if stripe_product == ENV['STRIPE_FREE_PLAN_ID']
-        max_goals = 1 # 1 plan per workspace
+        max_goals = 6 # 1 plan per workspace
       elsif stripe_product == ENV['STRIPE_PRO_PLAN_ID']
         max_goals = Float::INFINITY # unlimited plans per workspace
       else
         max_goals = 0 # no plans allowed
       end
     
-      if current_workspace.goals.count >= max_goals    
+      if plan.goals.count >= max_goals    
         return GraphQL::ExecutionError.new("You have reached the maximum number of goals allowed on the free plan")
       end
-
-      puts current_workspace.stripe_product
-      puts current_workspace.plans.count
-      puts current_workspace.goals.count
-      puts stripe_product
-      puts max_goals
     
       goal = plan.goals.create!(
         title: title, 
