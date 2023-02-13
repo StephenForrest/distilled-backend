fdebbec27ba13f0853c15a692471582630ca85c5
James <james.bohrman@fermyon.com>
2023-02-12 21:08:08 -0500

    restrictions

--- app/graphql/mutations/create_plan_mutation.rb
+++ app/graphql/mutations/create_plan_mutation.rb
@@ -9,9 +9,9 @@ module Mutations
     def resolve(name:)
       stripe_product = current_workspace.stripe_product
 
-      if stripe_product == "STRIPE_FREE_PLAN_ID"
+      if stripe_product == STRIPE_FREE_PLAN_ID
         max_plans = 1 # 1 plan per workspace
-      elseif stripe_product == "STRIPE_PRO_PLAN_ID"
+      elseif stripe_product == STRIPE_PRO_PLAN_ID
         max_plans = Float::INFINITY # unlimited plans per workspace
       else
         max_plans = 0 # no plans allowed
