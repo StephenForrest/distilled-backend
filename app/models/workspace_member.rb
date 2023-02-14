a3a2c5efd102138c278a39810b3bdee07c30f249
James <james.bohrman@fermyon.com>
2023-02-14 03:10:42 -0500

    commit

--- app/models/workspace_member.rb
+++ app/models/workspace_member.rb
@@ -21,6 +21,20 @@ class WorkspaceMember < ApplicationRecord
   belongs_to :workspace
   belongs_to :user
 
+  def user_restrictions
+
+    
+    customer = Stripe::Customer.retrieve(workspace.stripe_customer_id)
+    subscription = customer.subscriptions.data.first
+  
+    current_quantity = subscription.quantity
+  
+    if workspace.users = workspace.users.limit(max_users)
+      raise GraphQL::ExecutionError, "You have reached the maximum number of users for your plan. Please upgrade your plan to add more users."
+    end
+  end
+  
+
   enum :role, {
     admin: 0
   }
