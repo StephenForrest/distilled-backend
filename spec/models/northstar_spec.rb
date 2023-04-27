f8b3c1cba99d002b42e7992701740a1783e8b58d
James <james.bohrman@fermyon.com>
2023-04-27 04:09:00 -0400

    styling

--- spec/models/northstar_spec.rb
+++ spec/models/northstar_spec.rb
@@ -10,7 +10,7 @@
 require 'rails_helper'
 
 RSpec.describe Northstar, type: :model do
-    describe 'validations' do
-        it { should validate_presence_of(:title) }
-    end
+  describe 'validations' do
+      it { should validate_presence_of(:title) }
+  end
 end
